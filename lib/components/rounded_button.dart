d8d0c973c5cbfd774162fe5e8bd58bf10c1e6d0a
Anurag Bhandari <anurag.bhd@gmail.com>
2019-09-14 16:08:23 +0530

    Set text color of RoundedButton to white

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -21,6 +21,7 @@ class RoundedButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             this.text,
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
